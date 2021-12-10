8289ce01051dc973fb1d8c41e4e3556164ff493f
yillkid <yillkid@gmail.com>
2021-12-10 14:31:50 +0800

    fix: oauth refactory

--- static/js/GoogleOAuth.js
+++ static/js/GoogleOAuth.js
@@ -1,10 +1,29 @@
 function eIDLogin(id_token, res) {
-  console.log("hello, e-mail = " + res.result.emailAddresses[0].value);
-  console.log("hello, username = " + res.result.names[0].displayName);
+  setCookie("jwt", id_token, 1);
+  setCookie("email", res.result.emailAddresses[0].value, 1);
 
-  console.log("hello, token = " +  id_token);
-//  console.log("hello, access_token = " +  res.data.access);
-//  console.log("hello, refresh_token = " + res.data.refresh);
+  // Oauth to eID
+  var dataJSON = {};
+  dataJSON.email = res.result.emailAddresses[0].value;
+  dataJSON.username = res.result.names[0].displayName;
+  dataJSON.token = id_token
+  $.ajax({
+    url: HOST_URL_EID_DAEMON + "/accounts/oauth/google",
+    type: "POST",
+    async: false,
+    crossDomain: true,
+    data:  dataJSON,
+    success: function(returnData) {
+       const obj = JSON.parse(returnData);
+       // Set Cookie
+       setCookie("jwt", obj.token, 1);
+       setCookie("username", obj.username, 1);
+    },
+    error: function(xhr, ajaxOptions, thrownError){
+      console.log(thrownError);
+    }
+  });
+   window.location.replace("/eid.html");
 }
 
 //jQuery處理button click event 當畫面DOM都載入時....
